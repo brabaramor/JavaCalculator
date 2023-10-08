85105733b0c725bb115ac33a4ef65e6f825862f0
Barbara <128275198+brabaramor@users.noreply.github.com>
2023-10-08 03:43:08 -0300

    Ready!
    It's running perfectly on NetBeans!

---
+++ Javamavenproject1/src/main/java/com/mycompany/javamavenproject1/Javamavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javamavenproject1;
+
+/**
+ *
+ * @author mormata
+ */
+public class Javamavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
